86d5d272d14630a3a9b78aa6cf44b1af3c16f8fa
人間工作 <coolxiaom95@gmail.com>
2025-05-26 14:52:17 +0800

    Update Placeholders.java

--- src/main/java/top/mrxiaom/sweetdata/Placeholders.java
+++ src/main/java/top/mrxiaom/sweetdata/Placeholders.java
@@ -1,11 +1,15 @@
 package top.mrxiaom.sweetdata;
 
+import me.clip.placeholderapi.PlaceholderAPIPlugin;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import org.bukkit.OfflinePlayer;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import top.mrxiaom.pluginbase.utils.Util;
 import top.mrxiaom.sweetdata.database.PlayerDatabase;
 import top.mrxiaom.sweetdata.database.entry.PlayerCache;
+
+import java.util.Optional;
 
 public class Placeholders extends PlaceholderExpansion {
     private final SweetData plugin;
@@ -46,17 +50,46 @@ public class Placeholders extends PlaceholderExpansion {
             return request;
         }
         if (player != null) { // 玩家数值
-            PlayerDatabase db = plugin.getPlayerDatabase();
-            String[] split = params.substring(4).split(";", 2);
+            if (params.startsWith("$")) {
+                String[] split = params.substring(1).split("\\$", 2);
+                if (split.length == 1) return bool(false);
+                String[] conditionArray = split[0].split(":");
+
+                String[] split1 = params.split(";", 2);
+                String key = split1[0];
+                String def = split1.length == 2 ? split1[1] : "";
+                if ("range".equals(conditionArray[0]) && split.length == 3) {
+                    Integer rangeMin = Util.parseInt(conditionArray[1]).orElse(null);
+                    Integer rangeMax = Util.parseInt(conditionArray[1]).orElse(null);
+                    if (rangeMin == null || rangeMax == null) return bool(false);
+                    String str = get(player, key).orElse(def);
+                    Integer value = Util.parseInt(str).orElse(null);
+                    if (value == null) return bool(false);
+                    return bool(value >= rangeMin && value <= rangeMax);
+                }
+                return bool(false);
+            }
+
+            String[] split = params.split(";", 2);
             String key = split[0];
             String def = split.length == 2 ? split[1] : "";
-            PlayerCache cache = db.getCacheOrNull(player);
-            if (cache != null) {
-                return cache.get(key).orElse(def);
-            } else {
-                return db.get(player, key).orElse(def);
-            }
+
+            return get(player, key).orElse(def);
         }
         return null;
+    }
+
+    private Optional<String> get(@NotNull OfflinePlayer player, String key) {
+        PlayerDatabase db = plugin.getPlayerDatabase();
+        PlayerCache cache = db.getCacheOrNull(player);
+        if (cache != null) {
+            return cache.get(key);
+        } else {
+            return db.get(player, key);
+        }
+    }
+
+    public static String bool(boolean value) {
+        return value ? PlaceholderAPIPlugin.booleanTrue() : PlaceholderAPIPlugin.booleanFalse();
     }
 }
